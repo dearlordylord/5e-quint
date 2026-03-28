5e8e7613ccddc528fec639b4add82edc29647181
Igor Loskutov <igor.loskutoff@gmail.com>
2026-03-27 22:12:59 -0700

    fix: add dokku domains to vite preview allowedHosts

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -7,5 +7,8 @@ export default defineConfig({
   plugins: [tailwindcss(), react(), tsconfigPaths({ projects: ["./tsconfig.json"] })],
   server: {
     allowedHosts: ["determined_johnson.orb.local"]
+  },
+  preview: {
+    allowedHosts: ["dnd-quint.dearlordylord.com", "5e-quint.dearlordylord.com"]
   }
 })
